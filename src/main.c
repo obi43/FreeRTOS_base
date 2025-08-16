1a9bfcb26030ed19642de6b1a12e3a3dc32bf056
Torbjorn Birkeland <torbjornlundbi@gmail.com>
2025-08-16 18:19:24 +0200

    Updated kernel to work with RP2350 and added more examples

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 #ifdef PICO_W
 #include "pico/cyw43_arch.h"
@@ -12,6 +13,8 @@
 #ifndef PICO_W 
 const uint LED_PIN = 1;
 #endif
+
+SemaphoreHandle_t  xSemaphore; // The semaphore handle
 
 // The LED task. It blinks the LED, thats all.
 void led_task(void *arg)
@@ -24,7 +27,7 @@ void led_task(void *arg)
 #endif
 
     while(1)
-    {
+        {
 #ifdef PICO_W  
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, onOff);
 #else
@@ -32,7 +35,7 @@ void led_task(void *arg)
 #endif
         onOff = !onOff;
         vTaskDelay(1000);
-    }
+        }
 }
 
 
@@ -40,8 +43,23 @@ void led_task(void *arg)
 void hello_task(void *arg)
 {
     int taskNumber = (int)arg;
+
+
+
     while(1)
     {
+        // Take the semaphore, if it is not available then wait for 10 ticks
+        if( xSemaphoreTake( xSemaphore, ( TickType_t ) 10 ) == pdTRUE )
+        {
+            printf("Hello, world from task %d, and core %d!\n", taskNumber, get_core_num() );
+            vTaskDelay(1000);
+            xSemaphoreGive( xSemaphore );
+        }
+        else
+        {
+            printf("Task %d could not take the semaphore\n", taskNumber);
+        }
+    
         printf("Hello, world from task %d, and core %d!\n", taskNumber, get_core_num() );
         vTaskDelay(2000);
     }
@@ -51,24 +69,31 @@ int main()
 {
     stdio_init_all();
 
-#ifdef PICO_W  
-    if (cyw43_arch_init())
-    {   // Fatal error not able to continue
+    #ifdef PICO_W  
+        if (cyw43_arch_init())
+        {   // Fatal error not able to continue
+            goto fatalError;
+        }
+    #endif
+
+    // Attempt to create the semaphore
+    xSemaphore = xSemaphoreCreateBinary();
+    if (xSemaphore == NULL)
+    {
+        // There was insufficient FreeRTOS heap available for the semaphore to be created successfully
+        printf("Failed to create semaphore\n");
         goto fatalError;
     }
-#endif
 
     xTaskCreate(led_task, "ledTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(hello_task, "hellowTask", configMINIMAL_STACK_SIZE, (void*)1, 1, NULL);
     xTaskCreate(hello_task, "hellowTask", configMINIMAL_STACK_SIZE, (void*)2, 1, NULL);
 
-
     vTaskStartScheduler();
 
-
-// We shall newer ratch this point, but if we do then stay in an endless loop
-fatalError:
-    for (;;)
-        ;
+    // We shall newer ratch this point, but if we do then stay in an endless loop
+    fatalError:
+        for (;;)
+            ;
 
 }
